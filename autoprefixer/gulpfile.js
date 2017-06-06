bb7883c3140c17b7a5914baf04e2b2f1d323a573
Leonardo Souza <leogsouza@gmail.com>
2017-06-06 09:11:56 -0300

    Finish autoprefixer tutorial

--- autoprefixer/gulpfile.js
+++ autoprefixer/gulpfile.js
@@ -5,4 +5,8 @@ gulp.task('styles', function() {
   gulp.src('css/styles.css')
     .pipe(autoprefixer())
     .pipe(gulp.dest('build'));
-})
+});
+
+gulp.task('watch', function() {
+  gulp.watch('css/styles.css', ['styles']);
+});
